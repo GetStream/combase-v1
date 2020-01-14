7f4bcb16b754d0385b9c6d34652cdad501e37b92
Nick Parsons <nparsons08@gmail.com>
2020-01-14 15:44:37 +0100

    WIP

--- api/src/controllers/v1/organization/get.action.js
+++ api/src/controllers/v1/organization/get.action.js
@@ -1,11 +1,18 @@
 import Organization from '../../../models/organization';
 
+import StreamClient from '../../../utils/stream';
+
 exports.get = async (req, res) => {
 	try {
 		const data = { ...req.body, ...req.params };
 
-		const organization = await Organization.findById(data.organization);
-		res.status(200).json(organization);
+		const { key } = await StreamClient();
+
+		const organization = await Organization.findById(
+			data.organization
+		).lean();
+
+		res.status(200).json({ stream: { key }, ...organization });
 	} catch (error) {
 		res.status(500).json({ error: error.message });
 	}
--- api/src/utils/stream/index.js
+++ api/src/utils/stream/index.js
@@ -11,14 +11,20 @@ export default async () => {
 		// if this env is found, it's assumed that the api is running on heroku
 		if (process.env.STREAM_URL) {
 			// extract the key and secret from the environment variable
-			[ apiKey, apiSecret ] = process.env.STREAM_URL.substr(8).split('@')[0].split(':');
+			[apiKey, apiSecret] = process.env.STREAM_URL.substr(8)
+				.split('@')[0]
+				.split(':');
 		} else {
 			// api key and secret were provided from a .env file
 			apiKey = process.env.STREAM_API_KEY;
 			apiSecret = process.env.STREAM_API_SECRET;
 		}
 
-		const client = new StreamChat(apiKey, apiSecret);
+		const client = {
+			key: apiKey,
+			secret: apiSecret,
+			client: new StreamChat(apiKey, apiSecret),
+		};
 
 		return client;
 	} catch (error) {
