03ffc42a0f9831c3a03b4b91845b30a0e2067435
Nick Parsons <nparsons08@gmail.com>
2020-01-20 14:08:24 +0100

    User destroy

---
+++ api/src/controllers/v1/user/destroy.action.js
@@ -0,0 +1,23 @@
+import User from '../../../models/user';
+import Chat from '../../../models/chat';
+
+exports.destroy = async (req, res) => {
+	try {
+		const data = { ...req.body, ...req.params };
+		const serialized = req.serialized;
+
+		if (serialized.role !== 'admin') {
+			return res.status(403).json({
+				status: 'Invalid permissions to view or modify this resource.'
+			});
+		}
+
+		await User.findByIdAndRemove(data.user);
+		await Chat.remove({ 'refs.organization': data.organization });
+
+		res.sendStatus(204);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: error.message });
+	}
+};
