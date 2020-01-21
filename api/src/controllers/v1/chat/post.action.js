442766176f58237ab25802fa4dc9ccd51ae7d245
Nick Parsons <nparsons08@gmail.com>
2020-01-21 11:43:53 +0100

    WIP

--- api/src/controllers/v1/chat/post.action.js
+++ api/src/controllers/v1/chat/post.action.js
@@ -1,11 +1,25 @@
 import Chat from '../../../models/chat';
+import User from '../../../models/user';
 
 exports.post = async (req, res) => {
 	try {
-		const data = { ...req.body, ...req.params };
+		const { user, chat, organization } = req.body;
 
-		const chat = await Chat.create(data);
-		res.status(200).json(chat);
+		// if the user does not exist, create them in the database
+		await User.findOneOrCreate(
+			{ email: user.email },
+			{
+				name: {
+					first: user.name.first,
+					last: user.name.last
+				},
+				email: user.email,
+				organization: organization
+			}
+		);
+
+		const create = await Chat.create(chat);
+		res.status(200).json(create);
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: error.message });
