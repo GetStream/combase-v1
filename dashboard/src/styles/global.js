6a900dd558d027e32db8165034439958f2dad44e
Luke <luke@arkitek.co.uk>
2020-01-22 11:13:15 +0100

    global styles in styled-components

--- dashboard/src/styles/global.js
+++ dashboard/src/styles/global.js
@@ -8,36 +8,57 @@ export default createGlobalStyle`
     box-sizing: border-box;
     -webkit-tap-highlight-color: transparent;
   }
-
+  
   html {
     height: 100%;
+    overflow: hidden;
   }
-
+  
   body {
-    height: 100%;
     display: flex;
     background-color: ${({ theme }) => theme.color.background};
     margin: 0;
-    -webkit-font-smoothing: antialiased;
+    height: 100%;
+    overflow: hidden;
     font-family: "Circular Std", sans-serif;
+    -webkit-font-smoothing: antialiased;
   }
 
   #root {
-      position: relative;
-      display: flex;
-      align-items: stretch;
-      flex: 1;
-      height: 100%;
+    position: relative;
+    display: flex;
+    align-items: stretch;
+    overflow: hidden;
+    flex: 1;
+    height: 100%;
   }
 
-  div {
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      align-items: stretch;
-  }
-
-  p {
+  button {
+    font-family: inherit;
+    border: 0;
+    outline: 0;
     margin: 0;
+    padding: 0;
+  }
+  
+  h1, h2, h3,
+  h4, h5, h6 {
+    margin: 0;
+  }
+  
+  p,
+  label {
+    margin: 0;
+  }
+  
+  ul, ol {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  a {
+    text-decoration: none;
+    color: inherit;
   }
 `;
