fe9418cc02119c00a3734ad069bca4305d18b505
Luke <luke@arkitek.co.uk>
2020-01-22 11:31:36 +0100

    root component for dashboard

--- dashboard/src/screens/Dashboard/Dashboard.js
+++ dashboard/src/screens/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 // Data //
 import routes from "./routes";
@@ -6,8 +7,13 @@ import routes from "./routes";
 // HOCs //
 import withShell from "hocs/withShell";
 
+// Components //
+const Root = styled.div`
+  flex: 1;
+`;
+
 const Dashboard = () => {
-  return <p>dashboard</p>;
+  return <Root>dashboard</Root>;
 };
 
 export default withShell(Dashboard, routes);
