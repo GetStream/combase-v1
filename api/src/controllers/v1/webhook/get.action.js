e7d4d9259269039b681db6cdbdd5bf8a685660ef
Nick Parsons <nparsons08@gmail.com>
2020-01-23 14:40:18 +0100

    Cleanup"

---
+++ api/src/controllers/v1/webhook/get.action.js
@@ -0,0 +1,13 @@
+import Webhook from '../../../models/webhook';
+
+exports.get = async (req, res) => {
+	try {
+		const data = { ...req.body, ...req.params };
+
+		const webhook = await Webhook.findById(data.webhook);
+		res.status(200).json(webhook);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: error.message });
+	}
+};
---
+++ api/src/routes/webhook.js
@@ -0,0 +1,11 @@
+import { get, list, post, put, destroy } from '../controllers/v1/user';
+
+import { wrapAsync } from '../utils/controllers';
+
+module.exports = api => {
+	api.route('/v1/webhooks').get(wrapAsync(list));
+	api.route('/v1/webhooks/:webhook').get(wrapAsync(get));
+	api.route('/v1/webhooks/:webhook').put(wrapAsync(put));
+	api.route('/v1/webhooks/:webhook').post(wrapAsync(post));
+	api.route('/v1/webhooks/:webhook').delete(wrapAsync(destroy));
+};
