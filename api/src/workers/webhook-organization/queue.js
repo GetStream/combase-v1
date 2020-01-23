409b9f625ad7c802df53ea487d0c38d701ac4690
Amin Mahboubi <aminmahboobi@gmail.com>
2020-01-23 15:43:14 +0100

    fix webhook-organization worker

--- api/src/workers/webhook-organization/queue.js
+++ api/src/workers/webhook-organization/queue.js
@@ -1,12 +1,12 @@
 import Queue from 'bull';
 
-const WebhookAgentQueue = new Queue('webhook-agent', process.env.REDIS_URL, {
+const queue = new Queue('webhook-organization', process.env.REDIS_URL, {
 	limiter: { max: 1000, duration: 60000 }, // Limit to 1000 job per minute
 });
 
 // type: added || removed || updated
-export const AddToWebhookAgentQueue = async (type, data = {}) => {
-	return WebhookAgentQueue.add(type, data);
+export const AddToWebhookOrganizationQueue = async (type, data = {}) => {
+	return queue.add(type, data);
 };
 
-export default WebhookAgentQueue;
+export default queue;
