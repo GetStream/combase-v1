1bac78c8e89e0bc65f14cbf013bd5a33be4900c2
Nick Parsons <nparsons08@gmail.com>
2020-01-23 15:57:41 +0100

    Quick spelling error

--- api/src/workers/webhook-agent/index.js
+++ api/src/workers/webhook-agent/index.js
@@ -1,7 +1,7 @@
 import WebhookAgentQueue from './queue';
 import addedProcess from './added-process';
 import removedProcess from './removed-process';
-import updatedProcess from './added-process';
+import updatedProcess from './updated-process';
 
 WebhookAgentQueue.process('added', addedProcess);
 WebhookAgentQueue.process('removed', removedProcess);
