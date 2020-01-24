226867c84a1accb45d6d789dc42c40443fd62882
Luke <luke@arkitek.co.uk>
2020-01-24 13:38:28 +0100

    more customization options for Card components

--- dashboard/src/shared/Card.js
+++ dashboard/src/shared/Card.js
@@ -4,11 +4,16 @@ import styled from "styled-components";
 const Card = styled.div`
   border-radius: ${({ theme }) => theme.borderRadius}px;
   background-color: ${({ color, theme }) => theme.color[color]};
-  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.12);
+  box-shadow: ${({ flat }) =>
+    !flat ? "0px 4px 40px rgba(0, 0, 0, 0.12)" : null};
+  border: ${({ border, theme }) =>
+    border ? `1px solid ${theme.color.border}` : null};
 `;
 
 Card.defaultProps = {
-  color: "surface"
+  border: false,
+  color: "surface",
+  flat: false
 };
 
 export default Card;
