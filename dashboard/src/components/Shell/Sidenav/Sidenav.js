1d0f3d8af6a589ad026b359940502a134cd5181b
Luke <luke@arkitek.co.uk>
2020-01-24 16:57:06 +0100

    added logo

--- dashboard/src/components/Shell/Sidenav/Sidenav.js
+++ dashboard/src/components/Shell/Sidenav/Sidenav.js
@@ -1,54 +1,52 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 // Components //
-import SidenavItem from "./SidenavItem";
+import Logo from 'shared/Logo';
+import SidenavItem from './SidenavItem';
 
 const Root = styled.div`
-  display: none;
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 96px;
-  z-index: ${({ theme }) => theme.z.sidenav};
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 96px;
+    z-index: ${({ theme }) => theme.z.sidenav};
 
-  @media (min-width: ${({ theme }) => theme.breakpoints.sm}px) {
-    display: flex;
-  }
+    @media (min-width: ${({ theme }) => theme.breakpoints.sm}px) {
+        display: flex;
+    }
 `;
 
 const Brand = styled.div`
-  justify-content: center;
-  align-items: center;
-  padding: 24px 0px;
-`;
-
-const Logo = styled.div`
-  width: 56px;
-  height: 56px;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.color.gray};
+    justify-content: center;
+    align-items: center;
+    padding: 24px 0px;
 `;
 
 const Menu = styled.div`
-  flex: 1;
-  width: 100%;
-  margin-top: 24px;
+    flex: 1;
+    width: 100%;
+    margin-top: 24px;
 `;
 
 const renderItems = (routes, match) =>
-  routes.map(({ component, ...route }, key) => (
-    <SidenavItem {...{ key }} {...route} path={`${match.url}${route.slug}`} />
-  ));
+    routes.map(({ component, ...route }, key) => (
+        <SidenavItem
+            {...{ key }}
+            {...route}
+            path={`${match.url}${route.slug}`}
+        />
+    ));
 
 export default ({ match, routes }) => {
-  return (
-    <Root>
-      <Brand>
-        <Logo />
-      </Brand>
-      <Menu>{renderItems(routes, match)}</Menu>
-    </Root>
-  );
+    return (
+        <Root>
+            <Brand>
+                <Logo size={56} />
+            </Brand>
+            <Menu>{renderItems(routes, match)}</Menu>
+        </Root>
+    );
 };
