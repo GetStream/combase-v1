3aa4496e9efd2e7a12106d8c7670855239ac3487
Amin Mahboubi <aminmahboobi@gmail.com>
2020-01-30 20:54:36 +0100

    close redis connections

--- api/src/workers/bull-redis.js
+++ api/src/workers/bull-redis.js
@@ -2,16 +2,21 @@ import Redis from 'ioredis';
 
 const client = new Redis(process.env.REDIS_URL);
 const subscriber = new Redis(process.env.REDIS_URL);
+const defaults = [];
 
-const createClient = type => {
-	switch (type) {
-		case 'client':
-			return client;
-		case 'subscriber':
-			return subscriber;
-		default:
-			return new Redis(process.env.REDIS_URL);
-	}
+const closeRedisConnections = () => {
+	client.disconnect();
+	subscriber.disconnect();
+	defaults.forEach(c => c.disconnect());
 };
 
-export { createClient };
+const createClient = type => {
+	if (type === 'client') return client;
+	if (type === 'subscriber') return subscriber;
+
+	const newConnection = new Redis(process.env.REDIS_URL);
+	defaults.push(newConnection);
+	return newConnection;
+};
+
+export { createClient, closeRedisConnections };
