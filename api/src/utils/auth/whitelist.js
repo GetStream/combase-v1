6e78ce6aeb9f0a6722b7c0821f175065001ed26b
Nick Parsons <nparsons08@gmail.com>
2020-01-31 11:47:39 +0100

    Whitelist

---
+++ api/src/utils/auth/whitelist.js
@@ -0,0 +1,27 @@
+[
+	{
+		path: 'health',
+		method: 'GET',
+		auth: false,
+	},
+	{
+		path: 'auth',
+		method: 'POST',
+		auth: true,
+	},
+	{
+		path: 'configs',
+		method: 'GET',
+		auth: true,
+	},
+	{
+		path: 'password-reset',
+		method: 'POST',
+		auth: true,
+	},
+	{
+		path: 'organizations',
+		method: 'POST',
+		auth: true,
+	},
+];
