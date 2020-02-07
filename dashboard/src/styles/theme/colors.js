32f9f34ece9e4650d4dd597650ff144cacca4254
Luke <luke@arkitek.co.uk>
2020-02-07 15:28:20 -0700

    added todo to theme

--- dashboard/src/styles/theme/colors.js
+++ dashboard/src/styles/theme/colors.js
@@ -1,13 +1,15 @@
+// TODO: need more colors
+// for dark theme.
 export default {
-  blue: "#4D7CFE",
-  red: "#FF5B5B",
-  green: "#43FC99",
-  white: "#ffffff",
-  slate: "#606264",
-  light_gray: "#EFF1F5",
-  gray: "#C6C6C8",
-  off_white: "#FCFCFC",
-  black: "#302F32",
-  true_black: "#000000",
-  stream: "#4D7CFE",
+    blue: '#4D7CFE',
+    red: '#FF5B5B',
+    green: '#43FC99',
+    white: '#ffffff',
+    slate: '#606264',
+    light_gray: '#EFF1F5',
+    gray: '#C6C6C8',
+    off_white: '#FCFCFC',
+    black: '#302F32',
+    true_black: '#000000',
+    stream: '#4D7CFE',
 };
