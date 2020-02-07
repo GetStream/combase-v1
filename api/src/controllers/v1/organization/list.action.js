f00e3859b1d0f1677af65624c358c87d6ba315c3
Luke <luke@arkitek.co.uk>
2020-02-07 11:40:02 -0700

    removed agent check in list controller for organizations

--- api/src/controllers/v1/organization/list.action.js
+++ api/src/controllers/v1/organization/list.action.js
@@ -5,13 +5,6 @@ import Organization from 'models/organization';
 exports.list = async (req, res) => {
 	try {
 		const data = req.query;
-		const serialized = req.agent;
-
-		if (!serialized.admin) {
-			return res.status(403).json({
-				status: 'Invalid permissions to view or modify this resource.',
-			});
-		}
 
 		const organizations = await Organization.apiQuery(data);
 		res.status(200).json(organizations);
