757e1beb296b57f3b510ea161cb1eaa481c64f28
Nick Parsons <nparsons08@gmail.com>
2020-02-10 10:12:28 -0700

    Removed permissions on list view

--- api/src/controllers/v1/user/list.action.js
+++ api/src/controllers/v1/user/list.action.js
@@ -3,16 +3,15 @@ import User from 'models/user';
 exports.list = async (req, res) => {
 	try {
 		const data = req.query;
-		const serialized = req.agent;
-
-		if (!serialized.admin) {
-			return res.status(403).json({
-				status: 'Invalid permissions to view or modify this resource.',
-			});
-		}
 
 		const users = await User.apiQuery(data);
-		res.status(200).json(users);
+
+		const sanitized = users.map((user) => {
+			user.password = undefined;
+			return user;
+		});
+
+		res.status(200).json(sanitized);
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ error: error.message });
