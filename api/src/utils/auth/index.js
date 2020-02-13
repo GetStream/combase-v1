1d2f4070963343a30d92febe7811f4cba532700e
Luke <luke@arkitek.co.uk>
2020-02-12 18:28:14 -0700

    removed unnecesary async forEach helper

--- api/src/utils/auth/index.js
+++ api/src/utils/auth/index.js
@@ -6,12 +6,6 @@ import find from 'lodash.find';
 import Agent from 'models/agent';
 import whitelist from './whitelist';
 
-async function asyncForEach(array, callback) {
-	for (let index = 0; index < array.length; index++) {
-		await callback(array[index], index, array);
-	}
-}
-
 const auth = async (req, res, next) => {
 	try {
 		if (req.path.includes('health')) {
