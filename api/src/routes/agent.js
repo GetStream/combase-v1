3d3da77ba5a6fee073c6cdad8e1804bef86d5f05
Luke <luke@arkitek.co.uk>
2020-02-19 08:46:17 -0700

    fixed PUT route for /v1/agents

--- api/src/routes/agent.js
+++ api/src/routes/agent.js
@@ -1,11 +1,11 @@
-import { get, list, post, put, destroy } from '../controllers/v1/agent';
+import { get, list, post, update, destroy } from '../controllers/v1/agent';
 
 import { wrapAsync } from '../utils/controllers';
 
 module.exports = api => {
 	api.route('/v1/agents').get(wrapAsync(list));
 	api.route('/v1/agents/:agent').get(wrapAsync(get));
-	api.route('/v1/agents/:agent').put(wrapAsync(put));
+	api.route('/v1/agents/:agent').put(wrapAsync(update));
 	api.route('/v1/agents').post(wrapAsync(post));
 	api.route('/v1/agents/:agent').delete(wrapAsync(destroy));
 };
