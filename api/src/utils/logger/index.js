c55b75edd0a7398303aaa48f5ccbf5753357f951
Nick Parsons <nparsons08@gmail.com>
2020-02-24 11:43:40 -0700

    Remove papertrail

---
+++ api/src/utils/logger/index.js
@@ -0,0 +1,7 @@
+import winston from "winston/lib/winston/config";
+
+export default async () => {
+    const winstonPapertrail = new winston.transports.Papertrail({
+        host: process.env.
+    })
+};
