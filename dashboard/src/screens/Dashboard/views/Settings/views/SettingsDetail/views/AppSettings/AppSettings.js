f21f99c783ce03099632091fe70b96b5472c5e1e
Luke <luke@arkitek.co.uk>
2020-02-25 15:28:48 -0700

    fixed content alignment in the App Setting view

--- dashboard/src/screens/Dashboard/views/Settings/views/SettingsDetail/views/AppSettings/AppSettings.js
+++ dashboard/src/screens/Dashboard/views/Settings/views/SettingsDetail/views/AppSettings/AppSettings.js
@@ -11,6 +11,7 @@ import ShellContext from "contexts/Shell";
 // Components //
 import ListHeader from 'shared/ListHeader';
 import { AppSettingsIcon, SoundsIcon, ThemeIcon } from "shared/Icons";
+import { Col, Grid, Row } from 'shared/Grid';
 import Container from "shared/Container";
 import Switch from "components/Switch";
 import SettingsListItem from "components/SettingsListItem";
@@ -25,24 +26,34 @@ const AppSettings = () => {
   const { sounds } = useContext(ShellContext);
   return (
     <Root>
-      <Container maxWidth={640}>
+      <Container noPadding maxWidth={640}>
         {!isMobile ? <ListHeader bgColor="surface" showSearch={false} icon={AppSettingsIcon} title="App Settings" /> : null}
-        <SettingsListItem
-          icon={ThemeIcon}
-          color="text"
-          title="Dark Mode"
-          text="Toggle the UI theme of Combase."
-        >
-          <Switch checked={isDarkMode} onChange={toggleTheme} />
-        </SettingsListItem>
-        <SettingsListItem
-          icon={SoundsIcon}
-          color="yellow"
-          title="App Sounds"
-          text="Toggle the Notification & UI sounds."
-        >
-          <Switch checked={sounds.enabled} onChange={sounds.toggle} />
-        </SettingsListItem>
+        <Grid fluid>
+          <Row>
+            <Col>
+              <SettingsListItem
+                icon={ThemeIcon}
+                color="text"
+                title="Dark Mode"
+                text="Toggle the UI theme of Combase."
+              >
+                <Switch checked={isDarkMode} onChange={toggleTheme} />
+              </SettingsListItem>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <SettingsListItem
+                icon={SoundsIcon}
+                color="yellow"
+                title="App Sounds"
+                text="Toggle the Notification & UI sounds."
+              >
+                <Switch checked={sounds.enabled} onChange={sounds.toggle} />
+              </SettingsListItem>
+            </Col>
+          </Row>
+        </Grid>
       </Container>
     </Root>
   );
--- dashboard/src/shared/SectionTitle.js
+++ dashboard/src/shared/SectionTitle.js
@@ -7,9 +7,14 @@ import Text from 'shared/Text';
 const Root = styled.div`
     flex-direction: row;
     padding-bottom: 8px;
-    padding-left: 16px;
-    padding-right: 16px;
+    padding-left: 8px;
+    padding-right: 8px;
     border-bottom: 1px solid ${({ theme }) => theme.color.border};
+    
+    @media (min-width: ${({ theme }) => theme.breakpoints.sm}px){
+        padding-left: 16px;
+        padding-right: 16px;
+    }
 `;
 
 const SectionTitle = ({ className, title }) => (
