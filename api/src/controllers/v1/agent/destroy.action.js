735b6316f918ddd7e097ca8d6dbd73464a61dc9f
Luke <luke@arkitek.co.uk>
2020-02-26 09:59:23 -0700

    wip

--- api/src/controllers/v1/agent/destroy.action.js
+++ api/src/controllers/v1/agent/destroy.action.js
@@ -21,21 +21,21 @@ import Agent from 'models/agent';
    *         description: No response
    */
 exports.destroy = async (req, res) => {
-	try {
-		const { agent } = req.params;
-		const { serialized } = req;
+   try {
+      const { agent } = req.params;
+      const { serialized } = req;
 
-		if (serialized.role !== 'admin') {
-			return res.status(403).json({
-				status: 'Invalid permissions to view or modify this resource.'
-			});
-		}
+      if (serialized.role !== 'admin') {
+         return res.status(403).json({
+            status: 'Invalid permissions to view or modify this resource.'
+         });
+      }
 
-		const { password, ...removed } = await Agent.findByIdAndRemove(agent);
+      const { password, ...removed } = await Agent.findByIdAndRemove(agent);
 
-		res.sendStatus(204);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: error.message });
-	}
+      res.sendStatus(204);
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+   }
 };
