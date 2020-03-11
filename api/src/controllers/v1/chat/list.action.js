b4aeb202cd91acd0300b48d1d15189e34388db23
Luke Smetham <smeth@Lukes-MacBook-Pro.local>
2020-03-11 16:27:33 +0100

    removed auth check in chat list controller

--- api/src/controllers/v1/chat/list.action.js
+++ api/src/controllers/v1/chat/list.action.js
@@ -6,11 +6,11 @@ exports.list = async (req, res) => {
 	try {
 		const { serialized, query: data } = req;
 
-		if (serialized.role !== 'admin') {
-			return res.status(403).json({
-				status: 'Invalid permissions to view or modify this resource.',
-			});
-		}
+		// if (serialized.role !== 'admin') {
+		// 	return res.status(403).json({
+		// 		status: 'Invalid permissions to view or modify this resource.',
+		// 	});
+		// }
 
 		const chats = await Chat.apiQuery(data);
 		res.status(200).json(chats);
