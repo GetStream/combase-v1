f2d2ef0321ff43e264e4f82e56b058f4cc6b8ec3
Nick Parsons <nparsons08@gmail.com>
2020-03-17 14:34:59 -0600

    Updated create chat endpoint to return available and random agents.

--- api/src/controllers/v1/chat/post.action.js
+++ api/src/controllers/v1/chat/post.action.js
@@ -1,21 +1,45 @@
 import 'dotenv/config';
+import moment from 'moment';
 import { StreamChat } from 'stream-chat';
 
 import Chat from 'models/chat';
+import Agent from 'models/agent';
 import StreamClient from 'utils/stream';
 
 exports.post = async (req, res) => {
 	try {
 		const data = req.body;
 
-		const { agents: { assignee: { agent } }, organization, user } = data.refs;
+		const { organization, user } = data.refs;
 
-		const create = await Chat.create(data);
+		const agents = await Agent.find({ active: true }).lean();
+
+		const ts = moment().format('dddd H').split(' ');
+		const day = ts[0].toLowerCase();
+		const time = parseInt(ts[1], 10);
+
+		const available = agents
+			.map((agent) => {
+				const a = agent.availability[day];
+
+				if (a.enabled) {
+					if (time >= a.hours.from && time <= a.hours.to) {
+						return agent._id;
+					}
+				}
+
+				return [];
+			})
+			.flat(1);
+
+		const agent = available[Math.floor(Math.random() * available.length)];
+
+		const create = await Chat.create(...data, { agents: { assignee } });
 
 		const { key, secret } = await StreamClient();
 		const client = new StreamChat(key, secret);
 		const channel = client.channel('commerce', create._id.toString(), {
-			members: [agent, user],
+			members: [ agent, user ],
 			roles: {
 				agent: 'moderator',
 				user: 'channel_member'
@@ -28,7 +52,7 @@ exports.post = async (req, res) => {
 
 		const agentToken = client.createToken(agent);
 		const userToken = client.createToken(user);
-		console.log('channel', channel.state);
+
 		res.status(200).json({
 			...create,
 			tokens: {
