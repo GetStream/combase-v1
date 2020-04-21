f05ba4c2b5415e53cb8120ad10f8abd941ca5fb4
Luke Smetham <luke@arkitek.co.uk>
2020-04-21 12:47:01 +0200

    removed auth check from user delete controller

--- api/src/controllers/v1/user/destroy.action.js
+++ api/src/controllers/v1/user/destroy.action.js
@@ -4,13 +4,13 @@ import Chat from 'models/chat';
 exports.destroy = async (req, res) => {
 	try {
 		const data = { ...req.body, ...req.params };
-		const { serialized } = req;
+		// const { serialized } = req;
 
-		if (serialized.role !== 'admin') {
-			return res.status(403).json({
-				status: 'Invalid permissions to view or modify this resource.'
-			});
-		}
+		// if (serialized.role !== 'admin') {
+		// 	return res.status(403).json({
+		// 		status: 'Invalid permissions to view or modify this resource.'
+		// 	});
+		// }
 
 		await User.findByIdAndRemove(data.user);
 		await Chat.remove({
